235983ebdf4e992f75a822ac231b5b2d3cefc3cd
Matuges <d4rkdra@gmail.com>
2025-09-17 15:19:32 -0300

    Drag and drop and stock control
    Drag and drop to reorganize added and stock control

--- js/script.js
+++ js/script.js
@@ -7,21 +7,21 @@ function newProduct() {
     let name = document.querySelector('#product-name').value
     let brand = document.querySelector('#product-brand').value
     let price = document.querySelector('#product-price').value 
-
+    let stock = 0
     if (name && brand && price){
-
-    
 
     const newProductObject = {
         id: productIdCouter,
         name: name,
         brand: brand,
-        price: price
+        price: price,
+        stock: stock
     }
 
     document.querySelector('#product-name').value = ''
     document.querySelector('#product-brand').value = ''
     document.querySelector('#product-price').value = ''
+
     
 
     productsData.push(newProductObject)
@@ -37,19 +37,26 @@ function renderProductElement(product) {
     const div = document.createElement("div")
     div.className = "product-container"
     div.id = `${product.id}`
+    div.draggable = true
 
     div.innerHTML = `
         <div class = "product-actions"> 
         <button class = "edit" onclick="editProduct(${product.id})">Editar</button>
-        <button class="remove" onclick="del(${product.id})">X</button>
+        <div class = "stock-control"> 
+            <button class= "plus-stock" onclick="plusStock(${product.id})">+</button>
+            <button class= "subtraction-stock" onclick="subtractStock(${product.id})">-</button>
+        </div>
+        <button class="remove push" onclick="del(${product.id})">X</button>
+
         </div>
         <div class="name"> NOME DO PRODUTO: ${product.name}</div>
         <div class="brand">MARCA DO PRODUTO: ${product.brand}</div>
         <div class="price">PREÇO DO PRODUTO: R$${product.price}</div>
+        <div class="stock">ESTOQUE: ${product.stock}</div>
     `
     document.querySelector(".products").appendChild(div)
 
-
+    addDragAndDropListeners(div)
 }
 
 // FUNÇÃO PARA SALVAR NO LOCAL STORAGE
@@ -99,11 +106,13 @@ function editProduct (productId) {
     `
         <div class = "product-actions"> 
         <button class = "save" onclick="saveEditedProduct(${productId})">Salvar</button>
-        <button class="remove" onclick="del(${productId})">X</button>
+
+        <button class="remove push" onclick="del(${productId})">X</button>
         </div>
         <input type="text" class="edit-name" value="${productToEdit.name}">
         <input type="text" class="edit-brand" value="${productToEdit.brand}">
         <input type="number" class="edit-price" value="${productToEdit.price}">
+        <input type="number" class="edit-stock" value="${productToEdit.stock}">
     `
 }
 
@@ -113,11 +122,13 @@ function saveEditedProduct (productId) {
     const newName = productElement.querySelector(".edit-name").value
     const newBrand = productElement.querySelector(".edit-brand").value
     const newPrice = productElement.querySelector(".edit-price").value
+    const newStock = productElement.querySelector(".edit-stock").value
 
     const productToUpdate = productsData.find(product => product.id === productId)
     productToUpdate.name = newName
     productToUpdate.brand = newBrand
     productToUpdate.price = newPrice
+    productToUpdate.stock = newStock
 
     saveProductsToLocalStorage()
 
@@ -125,11 +136,17 @@ function saveEditedProduct (productId) {
     `
         <div class="product-actions">
             <button class="edit" onclick="editProduct(${productId})">Editar</button>
-            <button class="remove" onclick="del(${productId})">X</button>
+
+        <div class = "stock-control"> 
+            <button class= "plus-stock" onclick="plusStock(${productId})">+</button>
+            <button class= "subtraction-stock" onclick="subtractStock(${productId})">-</button>
+        </div>
+        <button class="remove push" onclick="del(${productId})">X</button>
         </div>
         <div class="name">NOME DO PRODUTO: ${productToUpdate.name}</div>
         <div class="brand">MARCA DO PRODUTO: ${productToUpdate.brand}</div>
         <div class="price">PREÇO DO PRODUTO: R$${productToUpdate.price}</div>
+        <div class="stock">ESTOQUE: ${productToUpdate.stock}</div>
     `;
 }
 
@@ -139,20 +156,33 @@ document.querySelector(".filterButton").addEventListener("click", filter)
 function filter() {
     let query = document.querySelector(".filterQuery").value
     const list = document.querySelector(".products")
-    if (query) {
+    const filterOption = document.querySelector('input[name="filter"]:checked')
+    if (filterOption) {
+        const id = filterOption.id
+        if (query && (id === "name")) {
 
-    list.innerHTML = ""
-    const filteredProducts = productsData.filter(product => {
-        return product.name.toLowerCase().indexOf(query.toLowerCase()) > -1
-    })
-
-     filteredProducts.forEach(product => {
-        renderProductElement(product)
-       })
-    } else {
         list.innerHTML = ""
-        loadProductsFromLocalStorage()
-        console.log("resetou")
+        const filteredProducts = productsData.filter(product => {
+            return product.name.toLowerCase().indexOf(query.toLowerCase()) > -1
+        })
+
+        filteredProducts.forEach(product => {
+            renderProductElement(product)
+        })
+        } else if (query && (id === "brand")){
+            list.innerHTML = ""
+        const filteredProducts = productsData.filter(product => {
+            return product.brand.toLowerCase().indexOf(query.toLowerCase()) > -1
+        })
+
+        filteredProducts.forEach(product => {
+            renderProductElement(product)
+        })
+        } else {
+            list.innerHTML = ""
+            loadProductsFromLocalStorage()
+            console.log("resetou")
+        }
     }
 }
 
@@ -182,5 +212,94 @@ function sort () {
     }
 }
 
+let draggedProductId = null;
 
-window.addEventListener('load', loadProductsFromLocalStorage)
+function addDragAndDropListeners(element) {
+    element.addEventListener('dragstart', handleDragStart)
+    element.addEventListener('dragover', handleDragOver)
+    element.addEventListener('dragleave', handleDragLeave)
+    element.addEventListener('drop', handleDragDrop)
+    element.addEventListener('dragend', handleDragEnd)
+}
+
+function handleDragStart(e) {
+    draggedProductId = e.target.id
+
+    e.target.classList.add("dragging")
+    e.dataTransfer.effectAllowed = 'move'
+    e.dataTransfer.setData('text/plain', e.target.id)
+}
+
+function handleDragOver(e) {
+    e.preventDefault()
+
+    if (e.target.closest('.product-container')) {
+        e.target.closest('.product-container').classList.add("drag-over")
+    }
+}
+
+function handleDragLeave (e) {
+
+     if (e.target.closest('.product-container')) {
+        e.target.closest('.product-container').classList.remove("drag-over")
+    }
+}
+
+function handleDragDrop (e) {
+    e.preventDefault()
+
+     if (e.target.closest('.product-container')) {
+        e.target.closest('.product-container').classList.remove("drag-over")
+    }
+
+    const targetElement = e.target.closest('.product-container')
+    if (!targetElement || targetElement.id === draggedProductId){
+        console.log ("teste")
+        return
+    }
+
+    const droppedOnProductId = targetElement.id
+
+    const draggedProductIndex = productsData.findIndex(p => p.id === draggedProductId)
+    const droppedOnProductIndex = productsData.findIndex(p => p.id === droppedOnProductId)
+
+    const [draggedProduct] = productsData.splice(draggedProductIndex, 1)
+    productsData.splice(droppedOnProductIndex, 0, draggedProduct)
+    document.querySelector(".products").innerHTML = "";
+    productsData.forEach(product => renderProductElement(product))
+    saveProductsToLocalStorage();
+    console.log ("dragconfirm")
+
+}
+
+function handleDragEnd(e) {
+    e.target.classList.remove("dragging")
+    draggedProductId = null
+}
+
+
+// STOCK CONTROL
+
+function plusStock (productId) {
+    const productToAdd = productsData.find(product => product.id === productId)
+    const element = document.getElementById(`${productId}`)
+    const stockElement = element.querySelector(".stock")
+
+    productToAdd.stock ++
+    stockElement.innerHTML = `ESTOQUE: ${productToAdd.stock}`
+    saveProductsToLocalStorage()
+    
+}
+
+function subtractStock (productId) {
+    const productToAdd = productsData.find(product => product.id === productId)
+    const element = document.getElementById(`${productId}`)
+    const stockElement = element.querySelector(".stock")
+
+    productToAdd.stock --
+    stockElement.innerHTML = `ESTOQUE: ${productToAdd.stock}`
+    saveProductsToLocalStorage()
+    
+}
+
+window.addEventListener('load', loadProductsFromLocalStorage) 
